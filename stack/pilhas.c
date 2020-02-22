b9f7bf212dc71a9c33e0e0d51b622c249ff38eb2
alan-va <61340645+alan-va@users.noreply.github.com>
2020-02-22 02:52:44 -0300

    Update pilhas.c

--- stack/pilhas.c
+++ stack/pilhas.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <error.h>
 #include "pilhas.h"
 
 #if (!defined INDEF) || (!defined pilhaVazia)
@@ -9,21 +10,23 @@
 
 void inicializaPilha(Pilha *P, int N)
 {
-  int i; if (N < 1) { return; }
-
-  P->valores = (int *) calloc(N, sizeof(int));
-
-  if (P->valores == NULL) 
-  { 
-    printf("Erro de alocação. (1)"); 
-    return;
+  int i; 
+  
+  if (N < 1)
+    error(0, 0, "inicializaPilha: quantidade invalida.");
+  else
+  {
+    P->topo = 0; P->tamanho = N;
+    P->valores = (int *) calloc(N, sizeof(int));
+    
+    if (P->valores == NULL) 
+      error(0, 0, "inicializaPilha: erro ao alocar pilha.");
+    else
+      for (i = 0; i <= N-1; i++) P->valores[i] = INDEF;
   }
-
-  for (i = 0; i <= N-1; i++) P->valores[i] = INDEF;
-  P->topo = 0; P->tamanho = N;
 } // fim - inicializaPilha
 
-void realocaPilha(Pilha *P, int N)
+static void realocaPilha(Pilha *P, int N)
 {
   int *valoresMod, newSize = P->tamanho + N;
   if (P->valores != NULL)
@@ -34,33 +37,42 @@ void realocaPilha(Pilha *P, int N)
       P->valores = valoresMod; 
       P->tamanho += N; return;
     }
-    else { printf("Erro na realocação. (2)"); return; }
+    else
+      error(0, 0, "realocaPilha: erro ao realocar a pilha.");
   }
-  else { printf("Pilha não alocada. (2)"); return; }
+  else
+    error(0, 0, "realocaPilha: pilha nao alocada.");
 } // fim - realocaPilha
 
 void empilha(Pilha *P, int valor)
 {
   /* para que a pilha seja realocada e nunca fique
    * cheia, mantenha a linha abaixo ativa. */
-  if (P->topo == P->tamanho) { realocaPilha(P, 1); }
+  if (P->topo == P->tamanho)
+    realocaPilha(P, 1);
   if (P->topo < P->tamanho)
   {
     P->valores[P->topo] = valor; P->topo++;
-    printf("\nEmpilhado com sucesso.\n");
-  } else { printf("\nFila cheia. (3)\n"); return; }
+    fprintf(stdout, "\nEmpilhado com sucesso.\n");
+  }
+  else
+    error(0, 0, "empilha: pilha cheia.");
 } // fim - empilha
 
 int desempilha(Pilha *P)
 {
   int noTopo;
-  if (P->topo == 0 || P->valores == NULL) { return INDEF; }
-  noTopo = P->valores[P->topo-1];
-  P->valores[P->topo-1] = INDEF; // desativa topo
-  /* para que a pilha seja realocada com um nodo a menos
-   * a cada remoção, mantenha a linha abaixo ativa.*/
-  if (P->tamanho > 1) { realocaPilha(P, -1); }
-  P->topo--; return P->valores[P->topo];
+  if (P->topo == 0 || P->valores == NULL)
+    error(0, 0, "desempilha: pilha vazia ou invalida.");
+  else
+  {
+    noTopo = P->valores[P->topo-1];
+    P->valores[P->topo-1] = INDEF; // desativa topo
+    /* para que a pilha seja realocada com um nodo a menos
+     * a cada remoção, mantenha a linha abaixo ativa.*/
+    if (P->tamanho > 1) realocaPilha(P, -1);
+    P->topo--; return noTopo;
+  }
 } // fim - desempilha
 
 int mostraTopo(Pilha *P)
@@ -71,21 +83,29 @@ int mostraTopo(Pilha *P)
 void mostraPilha(Pilha *P)
 {
   int i;
-  if (P->topo == 0 || 
-      P->valores == NULL) { printf("\nPilha vazia!\n"); return; }
-  for (i = 1; i <= P->topo; i++) 
-  { 
-    if (i == 1) 
+  if (P->topo == 0 || P->valores == NULL)
+    error(0, 0, "mostraPilha: pilha vazia ou invalida.");
+  else
+  {
+    for (i = 1; i <= P->topo; i++) 
     { 
-      printf("\n%.2d <- Topo\n", P->valores[P->topo-i]);
-      continue; 
+      if (i == 1) 
+      { 
+        printf("\n%.2d <- Topo\n", P->valores[P->topo-i]);
+        continue; 
+      }
+      printf("%.2d\n", P->valores[P->topo-i]); 
     }
-    printf("%.2d\n", P->valores[P->topo-i]); 
   }
 } // fim - mostraPilha
 
 void desalocaPilha(Pilha *P)
 {
-  P->topo = -1; P->tamanho = -1;
-  free(P->valores); P->valores == NULL;
+  if (P->valores != NULL)
+  {
+    P->topo = -1; P->tamanho = -1;
+    free(P->valores); P->valores == NULL;
+  }
+  else
+    error(0, 0, "desalocaPilha: pilha invalida");
 } // fim - desalocaPilha
